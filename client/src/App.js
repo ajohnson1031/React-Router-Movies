cd30359326d4d55f1a3662929440d9fb5e26f2cf
ajohnson1031 <aaron.johnson1031@gmail.com>
2020-11-06 14:29:43 -0500

    got movie list to display

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import MovieList from "./Movies/MovieList";
 
-import SavedList from './Movies/SavedList';
+import SavedList from "./Movies/SavedList";
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
-  const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+  const addToSavedList = (movie) => {
+    setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <MovieList />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
